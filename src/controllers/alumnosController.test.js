18ca9862c6d06ac66ba4847be5630e67892f6381
Jonatan <a17000799@alumnos.uayd.mx>
2024-03-05 13:22:38 -0600

    test lint

--- src/controllers/alumnosController.test.js
+++ src/controllers/alumnosController.test.js
@@ -3,8 +3,12 @@ import request from "supertest";
 import app from '../app.js';
 
 describe('Test API endpoints', () => {
+
   it('should return a 200 status code for the root endpoint', async () => {
+    
     const response = await request(app).get('/alumnos');
     expect(response.status).toBe(200);
+
   });
+
 });
